b20bcbd1ad387ffd4748016f272e60931ec1fcd3
Federico <cuervog992@gmail.com>
2023-01-14 20:09:12 -0300

    estilos formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,10 +3,13 @@ import { MARCAS, AÑOS, PLANES } from "../constants"
 
 const Formulario = () => {
   return (
-    <form className="mt-10">
-        <div>
-            <label>Marca</label>
-            <select name="marca">
+    <form className="mt-10 p-10">
+        <div className="my-5">
+            <label className="uppercase text-gray-400 font-bold py-10">Marca</label>
+            <select 
+                name="marca"
+                className="w-full p-2 border border-gray-400"
+            >
                 <option>-- Seleccione una marca --</option>
                 {
                     MARCAS.map(marca => (
@@ -20,9 +23,12 @@ const Formulario = () => {
                 }
             </select>
         </div>
-        <div>
-            <label>Año</label>
-            <select name="año">
+        <div className="my-5">
+            <label className="uppercase text-gray-400 font-bold py-10">Año</label>
+            <select 
+                name="año"
+                className="w-full p-2 border border-gray-400"
+            >
                 <option>-- Seleccione Año --</option>
                 {
                     AÑOS.map(año => (
@@ -37,9 +43,11 @@ const Formulario = () => {
             </select>
         </div>
         <div>
+            <p className="uppercase text-gray-400 font-bold">Plan</p>
+            <div className="flex gap-3">
             {
                 PLANES.map(plan => (
-                    <Fragment>
+                    <Fragment key={plan.id}>
                         <label>{plan.nombre}</label>
                         <input 
                             type="radio"
@@ -49,7 +57,13 @@ const Formulario = () => {
                     </Fragment>
                 ))
             }
+            </div>
         </div>
+        <input 
+            className="w-full bg-indigo-500 hover:bg-indigo-600 transition-colors text-white cursor-pointer p-3 font-bold uppercase"
+            type="submit"
+            value="Cotizar"
+        />
     </form>
   )
 }
